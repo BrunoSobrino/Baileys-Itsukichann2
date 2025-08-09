678696f946ec97beea59b09ed8dce84a4b38475e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2025-08-09 10:54:57 -0600

    Update Contact.d.ts

--- lib/Types/Contact.d.ts
+++ lib/Types/Contact.d.ts
@@ -1,13 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ContactAction = exports.Contact = void 0;
+
 export interface Contact {
-    id: string
-    lid?: string
-    jid?: string
+    /** ID either in lid or jid format */
+    id: string;
+    /** ID in Lid (anonymous) format (@lid) */
+    lid?: string;
+    /** ID in Phone Number format (@s.whatsapp.net) */
+    jid?: string;
     /** name of the contact, you have saved on your WA */
-    name?: string
+    name?: string;
     /** name of the contact, the contact has set on their own on WA */
-    notify?: string
+    notify?: string;
     /** I have no idea */
-    verifiedName?: string
+    verifiedName?: string;
     /**
      * Url of the profile picture of the contact
      *
@@ -15,6 +22,44 @@ export interface Contact {
      * null => if the profile picture has not been set (default profile picture)
      * any other string => url of the profile picture
      */
-    imgUrl?: string | null
-    status?: string
+    imgUrl?: string | null;
+    status?: string;
 }
+
+const Contact = {
+    /** ID either in lid or jid format */
+    id: '',
+    /** ID in Lid (anonymous) format (@lid) */
+    lid: undefined,
+    /** ID in Phone Number format (@s.whatsapp.net) */
+    jid: undefined,
+    /** name of the contact, you have saved on your WA */
+    name: undefined,
+    /** name of the contact, the contact has set on their own on WA */
+    notify: undefined,
+    /** I have no idea */
+    verifiedName: undefined,
+    /**
+     * Url of the profile picture of the contact
+     *
+     * 'changed' => if the profile picture has changed
+     * null => if the profile picture has not been set (default profile picture)
+     * any other string => url of the profile picture
+     */
+    imgUrl: undefined,
+    status: undefined
+};
+exports.Contact = Contact;
+
+export interface ContactAction {
+    firstName: string;
+    fullName: string;
+    saveOnPrimaryAddressbook: boolean;
+}
+
+const ContactAction = {
+    firstName: '',
+    fullName: '',
+    saveOnPrimaryAddressbook: false
+};
+exports.ContactAction = ContactAction;
